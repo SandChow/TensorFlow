e8944e778aa55451fcb28d7593ef3e4211640426
SandChow <srisandeep@hotmail.com>
2016-09-04 18:48:23 -0700

    Organise code

--- MNIST/src/beginnerMNIST.py
+++ MNIST/src/beginnerMNIST.py
@@ -1,26 +1,42 @@
+"""A very simple MNIST classifier.
+See extensive documentation at
+http://tensorflow.org/tutorials/mnist/beginners/index.md
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Import data
+from tensorflow.examples.tutorials.mnist import input_data
+
 import tensorflow as tf
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', '../data/MNIST_data/', 'Directory for storing data')
 
+mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+sess = tf.InteractiveSession()
+
+# Create the model
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-
-# Define Models
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-# Train
+# Define loss and optimizer
 y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-for i in range(1000):
-	batch_xs, batch_ys = mnist.train.next_batch(200)
-	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+# Train
+tf.initialize_all_variables().run()
+for i in range(1000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	train_step.run({x: batch_xs, y_: batch_ys})
+
+# Test trained model
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
