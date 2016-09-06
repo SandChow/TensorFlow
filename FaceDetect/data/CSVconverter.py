5f80f8157b2403ccc7c6b0548d902e17227994a1
SandChow <srisandeep@hotmail.com>
2016-09-05 18:49:16 -0700

    Debug errors

--- FaceDetect/data/CSVconverter.py
+++ FaceDetect/data/CSVconverter.py
@@ -1,12 +1,16 @@
 import os, argparse
 
 def convert(file):
-	with(open(file)) as f:
-		for line in f:
-			line = line.replace(" : ", ", ")
+	with(open(file, 'r+')) as f:
+		lines = f.readlines()
+		f.seek(0)
+		f.truncate()
+		for line in lines:
+			f.write(line.replace(" : ", ", "))
 
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description="Process a filepath.")
-	parser.add_argument("filepath", nargs=1,type="string", help="a filepath of file to be converted into csv format.")
+	parser.add_argument("filepath", help="a filepath of file to be converted into csv format.")
 	args = parser.parse_args()
 	convert(args.filepath)
+	os.system("mv " + args.filepath + " " + args.filepath.split(".")[0] + ".csv")
