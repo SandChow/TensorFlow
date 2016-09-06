5fb67eb561576d4ee78cc452c842c5cb64c760b6
sandchow <sandchow@uw.edu>
2016-09-06 13:06:43 -0700

    Update to add labels

--- FaceDetect/data/dataToTensor.py
+++ FaceDetect/data/dataToTensor.py
@@ -3,13 +3,19 @@ from scipy.misc import imread
 import pandas as pd
 
 def dataToTensor(tensorFile):
+	tensorFile.write("label, ")
+	for i in range(4095):
+		tensorFile.write("pixel" + str(i) + ", ")
+	tensorFile.write("pixel4095\n")
 	filePathFile = pd.read_csv('/sampa/home/sandchow/Desktop/TensorFlow/FaceDetect/data/trainFilePaths.csv')
+	counter = 0
 	for i in filePathFile.path:
+		tensorFile.write(str(filePathFile.label[counter]) + ", ")
+		counter += 1
 		im = imread(i).flatten()
 		for j in im[:-1]:
 			tensorFile.write(str(j) + ", ")
-		tensorFile.write(str(im[-1]))	
-		tensorFile.write('\n')
+		tensorFile.write(str(im[-1]) + '\n')
 
 
 if __name__=="__main__":
