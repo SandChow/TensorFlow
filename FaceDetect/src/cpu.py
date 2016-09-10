0266d30d45c83a55255ed365192142ef39d1f735
SandChow <srisandeep@hotmail.com>
2016-09-09 21:07:24 -0700

    Convert from notebook version to python script

--- FaceDetect/src/cpu.py
+++ FaceDetect/src/cpu.py
@@ -1,11 +1,171 @@
-import tensorflow as tf
-import pandas as pd
-import numpy as np
 
+import numpy as np
+import pandas as pd
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+# settings
+LEARNING_RATE = 1e-4
+TRAINING_ITERATIONS = 20000        
+BATCH_SIZE = 50
+
+# set to 0 to train on all available data
+VALIDATION_SIZE = 50
+
+# image number to output
+IMAGE_TO_DISPLAY = 52
+
+# read training data from CSV file 
 data = pd.read_csv('../data/train.csv')
+
 images = data.iloc[:,1:].values
+
+# convert to float for next step
 images = images.astype(np.float)
+
 # convert from [0:255] => [0.0:1.0]
 images = np.multiply(images, 1.0 / 255.0)
-image_size = images.shape[1]
-image_width = image_height = np.ceil(np.sqrt(image_size)).astype(np.uint8)
+
+labels_flat = data[[0]].values.ravel()
+
+# number of unique labels are tabulated. In this case, 1 and 0 = 2.
+labels_count = np.unique(labels_flat).shape[0]
+
+
+# convert class labels from scalars to one-hot vectors
+def dense_to_one_hot(labels_dense, num_classes):
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+labels = dense_to_one_hot(labels_flat, labels_count)
+labels = labels.astype(np.uint8)
+
+# split data into training & validation
+validation_images = images[:VALIDATION_SIZE]
+validation_labels = labels[:VALIDATION_SIZE]
+
+train_images = images[VALIDATION_SIZE:]
+train_labels = labels[VALIDATION_SIZE:]
+
+# placeholders and variables
+x = tf.placeholder(tf.float32, shape=[None, image_size])
+y_ = tf.placeholder(tf.float32, shape=[None, labels_count])
+W = tf.Variable(tf.zeros([4096,2]))
+b = tf.Variable(tf.zeros([2]))
+
+# initiate variables
+sess.run(tf.initialize_all_variables())
+
+# predicted class and loss function with training method
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
+# evaluation
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+predict = tf.argmax(y,1)
+
+epochs_completed = 0
+index_in_epoch = 0
+num_examples = train_images.shape[0]
+
+# serve data by batches
+def next_batch(batch_size):
+    
+    global train_images
+    global train_labels
+    global index_in_epoch
+    global epochs_completed
+    
+    start = index_in_epoch
+    index_in_epoch += batch_size
+    
+    # when all trainig data have been already used, it is reorder randomly    
+    if index_in_epoch > num_examples:
+        # finished epoch
+        epochs_completed += 1
+        # shuffle the data
+        perm = np.arange(num_examples)
+        np.random.shuffle(perm)
+        train_images = train_images[perm]
+        train_labels = train_labels[perm]
+        # start next epoch
+        start = 0
+        index_in_epoch = batch_size
+        assert batch_size <= num_examples
+    end = index_in_epoch
+    return train_images[start:end], train_labels[start:end]
+
+
+# visualisation variables
+train_accuracies = []
+validation_accuracies = []
+x_range = []
+
+display_step=1
+
+for i in range(TRAINING_ITERATIONS):
+
+    #get new batch
+    batch_xs, batch_ys = next_batch(BATCH_SIZE)        
+
+    # check progress on every 1st,2nd,...,10th,20th,...,100th... step
+    if i%display_step == 0 or (i+1) == TRAINING_ITERATIONS:
+        
+        train_accuracy = accuracy.eval(feed_dict={x:batch_xs, 
+                                                  y_: batch_ys})       
+        if(VALIDATION_SIZE):
+            validation_accuracy = accuracy.eval(feed_dict={ x: validation_images[0:BATCH_SIZE], 
+                                                            y_: validation_labels[0:BATCH_SIZE]})                                  
+            print('training_accuracy / validation_accuracy => %.2f / %.2f for step %d'%(train_accuracy, validation_accuracy, i))
+            
+            validation_accuracies.append(validation_accuracy)
+            
+        else:
+             print('training_accuracy => %.4f for step %d'%(train_accuracy, i))
+        train_accuracies.append(train_accuracy)
+        x_range.append(i)
+        
+        # increase display_step
+        if i%(display_step*10) == 0 and i:
+            display_step *= 10
+    # train on batch
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+if(VALIDATION_SIZE):
+    validation_accuracy = accuracy.eval(feed_dict={x: validation_images, 
+                                                   y_: validation_labels})
+
+# read test data from CSV file 
+test_images = pd.read_csv('../data/test.csv').values
+test_images = test_images.astype(np.float)
+
+# convert from [0:255] => [0.0:1.0]
+test_images = np.multiply(test_images, 1.0 / 255.0)
+
+# predict test set
+#predicted_lables = predict.eval(feed_dict={x: test_images, keep_prob: 1.0})
+
+# using batches is more resource efficient
+predicted_lables = np.zeros(test_images.shape[0])
+for i in range(0,test_images.shape[0]//BATCH_SIZE):
+    predicted_lables[i*BATCH_SIZE : (i+1)*BATCH_SIZE] = predict.eval(feed_dict={x: test_images[i*BATCH_SIZE : (i+1)*BATCH_SIZE]})
+
+predicted_answers = pd.read_csv('../data/submission_softmax.csv').iloc[:,1].values
+correct_answers = pd.read_csv('../data/answers.csv').iloc[:,0].values
+total = correct_answers.size
+correct = 0.0
+for i in range(total):
+    if correct_answers[i] == predicted_answers[i]:
+        correct += 1
+print 'accuracy => ' + str(correct/float(total))
